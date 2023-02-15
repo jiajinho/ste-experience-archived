5c7e1e0a02b32abf09fc0e6a69494066a2d6e493
jiajinho <hojiajin7@gmail.com>
2023-02-15 09:33:56 +0800

    Added missing chair

--- src/components/webgl/HellfireClub/groups/ChairGroup.tsx
+++ src/components/webgl/HellfireClub/groups/ChairGroup.tsx
@@ -40,6 +40,18 @@ export default () => {
         rotation-y={Math.PI}
         position={[1.18, 0, 5.21]}
       />
+
+      <Chair
+        scale={config.gltf.scale}
+        rotation-y={0.96}
+        position={[-10.72, 0, 0.92]}
+      />
+
+      <Chair
+        scale={config.gltf.scale}
+        rotation-y={-1.33}
+        position={[14.78, 0, 1.22]}
+      />
     </>
   )
 }
