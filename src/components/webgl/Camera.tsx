9a767af3ac0cf3bf4072590517e3138eb189f2c4
jiajinho <hojiajin7@gmail.com>
2023-02-17 09:24:49 +0800

    Added tween to camera position and rotation change

--- src/components/webgl/Camera.tsx
+++ src/components/webgl/Camera.tsx
@@ -1,16 +1,18 @@
 import { useEffect, useRef } from 'react';
-import { Vector3 } from 'three';
-import { PerspectiveCameraProps, useThree } from '@react-three/fiber';
+import gsap from 'gsap';
+import * as THREE from 'three';
+import { useThree } from '@react-three/fiber';
 import { useControls } from 'leva';
 
 import useCameraStore from 'store/useCameraStore';
 import config from 'config';
-import { PerspectiveCamera } from '@react-three/drei';
 
 export default () => {
   const { camera } = useThree();
 
-  const shadowCamera = useRef<PerspectiveCameraProps>(null);
+  const shadowCamera = useRef<THREE.PerspectiveCamera>(null);
+
+  const firstTime = useRef(true);
 
   const hotspot = useCameraStore(state => state.hotspot);
   const object = useCameraStore(state => state.object);
@@ -32,32 +34,48 @@ export default () => {
     if (object) {
       camera.lookAt(object.position);
     }
-
-    // camera.rotation.x = rx;
-    // camera.rotation.y = ry;
-    // camera.rotation.z = rz;
   }, [rx, ry, rz, x, y, z]);
 
   useEffect(() => {
-    camera.position.x = config.hotspots[hotspot][0];
-    camera.position.y = config.hotspots[hotspot][1];
-    camera.position.z = config.hotspots[hotspot][2];
+    if (!shadowCamera.current) return;
 
-    if (object) {
-      camera.lookAt(object.position);
-    }
-    else {
-      camera.lookAt(new Vector3(0, 0, 0));
-    }
+    const hotspotPosition = object ? object.position : new THREE.Vector3(0, 0, 0);
+
+    shadowCamera.current.position.x = config.hotspots[hotspot][0];
+    shadowCamera.current.position.y = config.hotspots[hotspot][1];
+    shadowCamera.current.position.z = config.hotspots[hotspot][2];
+
+    shadowCamera.current.lookAt(hotspotPosition);
+
+    const endRotation = new THREE.Euler().copy(shadowCamera.current.rotation);
+
+    const timeline = gsap.timeline({
+      defaults: {
+        duration: firstTime.current ? 0 : 1.5,
+        ease: "power2.out"
+      }
+    });
+
+    timeline
+      .to(camera.position, {
+        x: config.hotspots[hotspot][0],
+        y: config.hotspots[hotspot][1],
+        z: config.hotspots[hotspot][2]
+      })
+      .to(camera.rotation, {
+        x: endRotation.x,
+        y: endRotation.y,
+        z: endRotation.z,
+      }, "0");
+
+    firstTime.current = false;
   }, [hotspot]);
-
 
   useEffect(() => {
     console.log("camera position", camera.position);
   }, [camera.position.x, camera.position.y, camera.position.z]);
 
-
   return (
-    <PerspectiveCamera ref={shadowCamera} />
+    <perspectiveCamera ref={shadowCamera} />
   )
 }
