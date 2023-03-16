a10cf2c1028ace3bcf7ce70630b750eb59417c0f
jiajinho <hojiajin7@gmail.com>
2023-03-16 22:22:47 +0800

    Added video player on retro-tv

--- src/components/WebGL/HellfireClub/components/RetroTV.tsx
+++ src/components/WebGL/HellfireClub/components/RetroTV.tsx
@@ -1,12 +1,14 @@
 import React, { useRef } from "react";
+import styled from "styled-components";
 import type { GLTF } from "three-stdlib";
 import type { ThreeEvent } from "@react-three/fiber";
-import { useGLTF } from "@react-three/drei";
+import { Html, useGLTF } from "@react-three/drei";
 
 import useDebug from "../hooks/useDebug";
 import useZoom, { Zoom } from "../hooks/useZoom";
 
-const url = "/static/gltf/retro-tv.glb";
+const gltfUrl = "/static/gltf/retro-tv.glb";
+const videoUrl = "/static/mightyverse-teaser.mp4#t=0.001";
 
 type GLTFResult = GLTF & {
   nodes: {
@@ -17,11 +19,27 @@ type GLTFResult = GLTF & {
   };
 };
 
+const Screen = styled.div`
+  position: relative;
+  background: black;
+  width: 1160px;
+  height: 910px;
+
+  & > video {
+    position: absolute;
+    top: 50%;
+    left: 0;
+    transform: translateY(-50%);
+    width: 100%;
+    height: 80%;
+  }
+`;
+
 export default ({ zoom, ...props }: {
   zoom: Zoom
 } & JSX.IntrinsicElements["group"]
 ) => {
-  const { nodes, materials } = useGLTF(url) as any as GLTFResult;
+  const { nodes, materials } = useGLTF(gltfUrl) as any as GLTFResult;
   const ref = useRef<THREE.Group>(null);
 
   const triggerMover = useDebug(ref);
@@ -40,13 +58,31 @@ export default ({ zoom, ...props }: {
       onClick={handleClick}
       dispose={null}
     >
+      <Html
+        occlude="blending"
+        prepend
+        transform
+        position={[0.165, 0, 0]}
+        rotation-y={Math.PI / 2}
+        scale={0.01}
+      >
+        <Screen>
+          <video controls autoPlay={false}>
+            <source
+              src={videoUrl}
+              type="video/mp4"
+            />
+          </video>
+        </Screen>
+      </Html>
+
       <mesh
         castShadow
         geometry={nodes.RetroTV.geometry}
         material={materials.RetroTV}
       />
-    </group>
+    </group >
   );
 }
 
-useGLTF.preload(url);
+useGLTF.preload(gltfUrl);
