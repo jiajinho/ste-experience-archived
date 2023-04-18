153c0bd01017f1e4db4d74754f6314159b5554ac
jiajinho <hojiajin7@gmail.com>
2023-04-18 20:52:11 +0800

    Animating card will now prevent navigating to another hotspot

--- src/components/WebGL/Camera/useZoomHotspot.ts
+++ src/components/WebGL/Camera/useZoomHotspot.ts
@@ -3,16 +3,21 @@ import { useEffect, useRef } from 'react';
 
 import { moveCamera } from './utils';
 import useCameraStore from 'stores/webgl/useCameraStore';
+import useCardStore from 'stores/html/useCardStore';
 
 export default () => {
   const firstTime = useRef(true);
 
+  const cardHtmlEvent = useCardStore(state => state.htmlEvent);
+  const cardWebglEvent = useCardStore(state => state.webglEvent);
   const { shadowCamera, currentZoom, camera } = useCameraStore(state => state);
   const setCameraStore = useCameraStore(state => state.set);
 
   useEffect(() => {
     if (!shadowCamera) return;
     if (!camera) return;
+    if (!!cardHtmlEvent) return;
+    if (!!cardWebglEvent) return;
 
     const { lookAt, cameraPosition, cameraUp } = config.zoomSettings[currentZoom];
 
