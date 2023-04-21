adcda53ce714aa20449b69d83307751c4380126c
jiajinho <hojiajin7@gmail.com>
2023-04-21 15:27:08 +0800

    Added polaroid

---
+++ src/components/WebGL/HellfireClub/components/Polaroid.tsx
@@ -0,0 +1,52 @@
+import React, { useRef } from "react";
+import { Image, useGLTF } from "@react-three/drei";
+import type { ThreeEvent } from "@react-three/fiber";
+import type { GLTF } from "three-stdlib";
+
+import useTriggerDebugModel from '@webgl/debug/hooks/useTriggerDebugModel';
+
+const url = "/static/gltf/polaroid.glb";
+
+type GLTFResult = GLTF & {
+  nodes: {
+    Polaroid: THREE.Mesh;
+  };
+  materials: {
+    polaroid: THREE.MeshStandardMaterial;
+  };
+};
+
+export default (props: JSX.IntrinsicElements["group"]) => {
+  const { nodes, materials } = useGLTF(url) as any as GLTFResult;
+  const ref = useRef<THREE.Group>(null);
+
+  const triggerMover = useTriggerDebugModel(ref);
+
+  const handleClick = (e: ThreeEvent<MouseEvent>) => {
+    triggerMover();
+    props.onClick && props.onClick(e);
+  }
+
+  return (
+    <group
+      ref={ref}
+      {...props}
+      onClick={handleClick}
+      dispose={null}
+    >
+      <mesh
+        geometry={nodes.Polaroid.geometry}
+        material={materials.polaroid}
+      />
+
+      <Image
+        url='https://upload.wikimedia.org/wikipedia/commons/4/40/Sunflower_sky_backdrop.jpg'
+        scale={0.21}
+        position-y={0.02}
+        rotation-y={Math.PI / 2}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload(url);
