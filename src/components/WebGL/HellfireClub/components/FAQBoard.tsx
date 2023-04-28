93fa251162b3431597d81f7dd3bbfcb92f5c1818
jiajinho <hojiajin7@gmail.com>
2023-04-28 11:28:42 +0800

    Reworked vecna and faq board texture

--- src/components/WebGL/HellfireClub/components/FAQBoard.tsx
+++ src/components/WebGL/HellfireClub/components/FAQBoard.tsx
@@ -13,8 +13,8 @@ type GLTFResult = GLTF & {
     FAQBoard: THREE.Mesh;
   };
   materials: {
-    ["cta.001"]: THREE.MeshStandardMaterial;
-    ["FAQBoard.001"]: THREE.MeshStandardMaterial;
+    cta: THREE.MeshStandardMaterial;
+    FAQBoard: THREE.MeshStandardMaterial;
   };
 };
 
@@ -41,12 +41,12 @@ export default ({ onCallToAction, ...props }: {
     >
       <mesh
         geometry={nodes.cta.geometry}
-        material={materials["cta.001"]}
+        material={materials.cta}
         onClick={onCallToAction}
       />
       <mesh
         geometry={nodes.FAQBoard.geometry}
-        material={materials["FAQBoard.001"]}
+        material={materials.FAQBoard}
       />
     </group>
   );
