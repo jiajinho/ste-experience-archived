f8171a5977f78f48f8cc6b30df49dd91fd2e4502
jiajinho <hojiajin7@gmail.com>
2023-04-28 16:07:40 +0800

    Added paper on tabletop

--- src/components/WebGL/HellfireClub/groups/TabletopGroup.tsx
+++ src/components/WebGL/HellfireClub/groups/TabletopGroup.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 
 import MeetingTable from '@hellfire/components/MeetingTable';
+import Page from '../components/Page';
 
 export default () => {
   return (
     <>
       <MeetingTable
         position={[0, 0.68, 0]}
+      />
+
+      <Page.Demogorgon
+        position={[-0.13, 0.825, 0.71]}
+        rotation-y={3.45}
+      />
+
+      <Page.Rafflesia
+        position={[-0.26, 0.82, 0.68]}
+        rotation-y={-0.43}
       />
     </>
   )
