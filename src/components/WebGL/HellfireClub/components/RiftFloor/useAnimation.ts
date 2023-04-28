8c09d390c6240e58a1dc0b5366fde1a28d77bf41
jiajinho <hojiajin7@gmail.com>
2023-04-28 14:38:21 +0800

    Reworked RiftLight, now the spotlight is part of the model and animation

---
+++ src/components/WebGL/HellfireClub/components/RiftFloor/useAnimation.ts
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { gsap } from 'gsap';
+
+export default (material: THREE.MeshStandardMaterial, spotlight: React.RefObject<THREE.SpotLight>) => {
+
+  useEffect(() => {
+    if (!spotlight.current) return;
+
+    const ogEmissiveIntensity = 20;
+
+    material.emissiveIntensity = ogEmissiveIntensity;
+    material.toneMapped = false;
+
+    const timeline = gsap.timeline({
+      ease: "power2.out",
+      yoyo: true,
+      repeat: -1,
+      repeatDelay: 3
+    });
+
+    timeline.to(material, {
+      duration: 3,
+      emissiveIntensity: 4,
+    }).to(spotlight.current, {
+      duration: 3,
+      intensity: 1
+    }, 0);
+
+    return () => { timeline.kill() }
+  }, [material]);
+
+}
