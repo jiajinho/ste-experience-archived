185fc4620921e0d811c852d499747f1945b0552d
jiajinho <hojiajin7@gmail.com>
2023-05-22 21:01:18 +0800

    Now properly returns mixpanel error

--- src/pages/api/mixpanel.ts
+++ src/pages/api/mixpanel.ts
@@ -21,12 +21,19 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const browser = ua.getBrowser();
   const ip = requestIp.getClientIp(req);
 
+  const callback = (e?: Error) => {
+    if (e) {
+      res.status(500).json({ message: e.message });
+    }
+    else {
+      res.status(200).json(true);
+    }
+  }
+
   mixpanel.track(req.query.event as string, {
     $ip: ip,
     $os: `${os.name} ${os.version}`,
     $browser: browser.name,
     $browser_version: browser.version
-  });
-
-  res.status(200).json(true);
+  }, callback);
 }
