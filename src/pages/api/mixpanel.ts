39133e3708dcf2cad2bae9493adf6550ebbafbe3
jiajinho <hojiajin7@gmail.com>
2023-05-22 17:33:59 +0800

    Fixed mixpanel to not cache on SSR

--- src/pages/api/mixpanel.ts
+++ src/pages/api/mixpanel.ts
@@ -1,19 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import Mixpanel from "mixpanel";
 import UAParser from "ua-parser-js";
+import requestIp from "request-ip";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (!process.env.MIXPANEL_TOKEN) throw new Error("Undefined MIXPANEL_TOKEN");
   if (!req.query.event) return;
 
-  const mixpanel = Mixpanel.init(process.env.MIXPANEL_TOKEN, { keepAlive: false });
+  const mixpanel = Mixpanel.init(process.env.MIXPANEL_TOKEN, {
+    api_method: "POST"
+  });
 
   const ua = new UAParser(req.headers["user-agent"]);
   const os = ua.getOS();
   const browser = ua.getBrowser();
+  const ip = requestIp.getClientIp(req);
 
   mixpanel.track(req.query.event as string, {
-    $ip: req.socket.remoteAddress,
+    $ip: ip,
     $os: `${os.name} ${os.version}`,
     $browser: `${browser.name} ${browser.version}`,
   });
