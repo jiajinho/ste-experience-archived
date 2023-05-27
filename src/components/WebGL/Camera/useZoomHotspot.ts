f5297bf8e9f56faad966983976daa3b88ccebdd0
jiajinho <hojiajin7@gmail.com>
2023-05-27 22:57:49 +0800

    Refactor useZoomHotspot

--- src/components/WebGL/Camera/useZoomHotspot.ts
+++ src/components/WebGL/Camera/useZoomHotspot.ts
@@ -1,34 +1,34 @@
 import { useEffect, useRef } from 'react';
-import { useThree } from '@react-three/fiber';
 
 import config from 'config';
 import api from 'api';
 import { MixpanelEvent } from 'api/mixpanel';
 import { moveCamera } from './utils';
+
 import useCameraStore from 'stores/webgl/useCameraStore';
-import useCardStore from 'stores/html/useCardStore';
 import useEnvStore from 'stores/useEnvStore';
 
 export default () => {
   const firstTime = useRef(true);
-  const { aspect } = useThree(state => state.viewport);
 
   const env = useEnvStore(state => state.env);
-  const cardHtmlEvent = useCardStore(state => state.htmlEvent);
-  const cardWebglEvent = useCardStore(state => state.webglEvent);
-  const { shadowCamera, currentZoom, camera } = useCameraStore(state => state);
+  const shadowCamera = useCameraStore(state => state.shadowCamera);
+  const currentZoom = useCameraStore(state => state.currentZoom);
+  const camera = useCameraStore(state => state.camera);
+
   const setCameraStore = useCameraStore(state => state.set);
 
   useEffect(() => {
     if (env === "development") return;
     if (!shadowCamera) return;
     if (!camera) return;
-    if (!!cardHtmlEvent) return;
-    if (!!cardWebglEvent) return;
 
     const setting = config.zoomSettings[currentZoom];
 
-    if (!setting.lookAt || !setting.cameraPosition) return;
+    if (!setting.lookAt) return;
+    if (!setting.cameraPosition) return;
+
+    const allowEventName = config.zoomSettings[currentZoom].allowEvent?.name;
 
     setCameraStore("mouseEvent", undefined);
 
@@ -40,12 +40,12 @@ export default () => {
       up: setting.cameraUp,
       animate: !firstTime.current,
       callback: () => {
-        setCameraStore("mouseEvent", config.zoomSettings[currentZoom].allowEvent?.name);
+        setCameraStore("mouseEvent", allowEventName);
       }
     });
 
     firstTime.current = false;
-  }, [currentZoom, camera, shadowCamera, aspect, env]);
+  }, [currentZoom, camera, shadowCamera, env]);
 
   useEffect(() => {
     switch (currentZoom) {
