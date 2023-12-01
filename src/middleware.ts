629474dafc1d621db52317aa410e693cd705b996
jiajinho <hojiajin7@gmail.com>
2023-12-01 22:19:33 +0800

    removed edge function

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,17 +6,19 @@ export async function middleware(request: NextRequest) {
   if (process.env.NODE_ENV !== "production") return NextResponse.next();
 
   //Production
-  const maintenance = await get<boolean>('steEncounterIsInMaintenanceMode');
+  // const maintenance = await get<boolean>('steEncounterIsInMaintenanceMode');
 
-  if (maintenance && request.nextUrl.pathname !== "/maintenance") {
-    return NextResponse.redirect(`${request.nextUrl.origin}/maintenance`);
-  }
-  else if (!maintenance && request.nextUrl.pathname === "/maintenance") {
-    return NextResponse.redirect(request.nextUrl.origin);
-  }
-  else {
-    return NextResponse.next();
-  }
+  // if (maintenance && request.nextUrl.pathname !== "/maintenance") {
+  //   return NextResponse.redirect(`${request.nextUrl.origin}/maintenance`);
+  // }
+  // else if (!maintenance && request.nextUrl.pathname === "/maintenance") {
+  //   return NextResponse.redirect(request.nextUrl.origin);
+  // }
+  // else {
+  //   return NextResponse.next();
+  // }
+
+  return NextResponse.next();
 }
 
 export const config = {
